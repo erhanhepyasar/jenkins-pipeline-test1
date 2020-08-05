ad2b4e9eb249bc14018bce3b13c4fc8aa5f618b5
Erhan Hepyasar <erhan.hepyasar@gmail.com>
2020-08-05 17:05:28 +0300

    commit

--- MyApp.java
+++ MyApp.java
@@ -2,5 +2,6 @@ class MyApp {
     public static void main(String[] args) {
         System.out.println("Simple project for Jenkins test");
 		System.out.println("Github commit triggered Jenkins job");
+        System.out.println("Lets try again");
     }
 }
