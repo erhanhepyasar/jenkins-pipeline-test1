7ea4685966086e8ff54c0c17f1efbcfe6df98fd4
Erhan Hepyasar <erhan.hepyasar@gmail.com>
2020-08-06 05:08:11 +0300

    hello3

--- MyApp.java
+++ MyApp.java
@@ -1,5 +1,5 @@
 class MyApp {
     public static void main(String[] args) {        
-        System.out.println("----- Hello Jenkins2 -----");
+        System.out.println("----- Hello Jenkins3 -----");
     }
 }
