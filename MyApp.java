56876edcb0c1a6d3c0e609228fd9e7b73fa3ac87
Erhan Hepyasar <erhan.hepyasar@gmail.com>
2020-08-06 04:20:02 +0300

    hello

--- MyApp.java
+++ MyApp.java
@@ -1,9 +1,5 @@
 class MyApp {
-    public static void main(String[] args) {
-        System.out.println("Simple project for Jenkins test");
-        System.out.println("Lets try again");
-        System.out.println("-----Last line added -----");
-        System.out.println("----- I was kidding :) -----");
-        System.out.println("----- Hello again -----");
+    public static void main(String[] args) {        
+        System.out.println("----- Hello Jenkins -----");
     }
 }
