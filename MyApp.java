305ae18a7d4374042aaa1abea2867c16e75ee4e1
Erhan Hepyasar <erhan.hepyasar@gmail.com>
2020-08-06 04:10:14 +0300

    kidding

--- MyApp.java
+++ MyApp.java
@@ -1,9 +1,8 @@
 class MyApp {
     public static void main(String[] args) {
         System.out.println("Simple project for Jenkins test");
-		System.out.println("Github commit triggered Jenkins job");
         System.out.println("Lets try again");
         System.out.println("-----Last line added -----");
-
+        System.out.println("----- I was kidding :) -----");
     }
 }
